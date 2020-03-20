12a165576e2d38a327756892fff62b6fd35b2c63
eunjoon <joonybejoy@gmail.com>
2020-03-20 16:38:27 -0400

    cnahge popup.html

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const path = require('path');
-const io = require('socket.io')(server)
+const app = require("express")();
+const server = require("http").createServer(app);
+const path = require("path");
+const io = require("socket.io")(server);
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/../extension/popup.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/../extension/popup.html"));
+  //res.sendFile(path.join(__dirname, "/popup.html"));
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("chat message", function(msg) {
+    io.emit("chat message: ", msg);
   });
-  
-  server.listen(3000, function(){
-    console.log('listening on *:3000');
-  });
+});
 
-module.exports = io; 
+server.listen(3000, function() {
+  console.log("listening on *:3000");
+});
+
+module.exports = io;
