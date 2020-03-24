1974b6f0dd9a8a41e04660ba6e7c6cba334c0cec
eunjoon <joonybejoy@gmail.com>
2020-03-24 18:44:15 -0400

    sending messages to react

--- app/root.js
+++ app/root.js
@@ -1,31 +1,57 @@
-import React from 'react';
-import socketIOClient from 'socket.io-client';
-
-const socket = socketIOClient('http://localhost:3000');
+import React from "react";
+import socketIOClient from "socket.io-client";
 
 export default class Root extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
+      username: "",
+      message: "",
       messages: []
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    };
+    this.socket = socketIOClient("http://localhost:3000");
+    this.socket.on("receive message", function(msg) {
+      addMessage(msg);
+    });
 
-  handleSubmit(event) {
-    event.preventDefault();
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+    const addMessage = msg => {
+      console.log("data_________", msg);
+      this.setState({ messages: [...this.state.messages, msg] });
+      console.log("state_________", this.state);
+    };
+
+    this.sendMessage = event => {
+      event.preventDefault();
+      this.socket.emit("chat message", {
+        author: this.state.username,
+        message: this.state.message
+      });
+      this.setState({ message: "" });
+    };
   }
 
   render() {
+    console.log("I am here", this.state);
     return (
       <div>
-        <ul id="messages"></ul>
+        <ul id="messages">
+          {this.state.messages.map(message => {
+            return (
+              <li>
+                {message.author}: {message.message}
+              </li>
+            );
+          })}
+        </ul>
         <form action="">
-          <input id="m" autocomplete="off" />
-          <button onClick={this.handleSubmit}>Send</button>
+          <input
+            type="text"
+            placeholder="Message"
+            value={this.state.message}
+            onChange={ev => this.setState({ message: ev.target.value })}
+            className="form-control"
+          />
+          <button onClick={this.sendMessage}>Send</button>
         </form>
         {/* {function() {
           var socket = io();
