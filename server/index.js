3bc6d984fbd218b37950ac2069c170b2832171be
Dani <dani.meyer.nd@gmail.com>
2020-03-24 11:43:35 -0400

    refactor: separated extension into another repo

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
-const app = require("express")();
-const server = require("http").Server(app);
-const path = require("path");
-const io = require("socket.io")(server);
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const path = require('path');
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../extension/socket.html"));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/../public/socket.html'));
   //res.sendFile(path.join(__dirname, "/popup.html"));
 });
 
-io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("chat message", function(msg) {
-    io.emit("chat message", msg);
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
   });
 });
 
