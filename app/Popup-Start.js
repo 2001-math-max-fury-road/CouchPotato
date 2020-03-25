37ee5e8b2002b7e309a74a1cdb3b70d0ebf11075
eunjoon <joonybejoy@gmail.com>
2020-03-25 18:10:17 -0400

    chore:popup room redirect

--- app/Popup-Start.js
+++ app/Popup-Start.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import axios from 'axios'
-import randomizeRoomId from '../server/utils'
+import React from "react";
+import axios from "axios";
 
 export default class StartPopup extends React.Component {
-    constructor() {
-        super()
-        this.state = {username: ''}
-        this.startCouch = this.startCouch.bind(this)
-    }
+  constructor() {
+    super();
+    this.state = { username: "" };
+    this.startCouch = this.startCouch.bind(this);
+  }
 
-    startCouch(event) {
-        this.setState({username: event.target.value})
-        const roomId = randomizeRoomId()
-        const {data} = axios.post('/api/', roomId)
+  async startCouch(event) {
+    event.preventDefault();
+    this.setState({ username: event.target.value });
+    const { data } = await axios.post("/api/");
+    console.log("data_______", data.roomId);
+    location.replace(`http://localhost:3000/${data.roomId}`);
+  }
 
-    }
-
-    render() {
-        return (
-            <div className='popup'>
-                <div className='popup\_inner'>
-                    <form id='popup-form'>
-                    <label htmlFor="username">Your Name: </label>
-                    <input name='username'></input>
-                    <button onClick={this.startCouch}>Start New Couch</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="popup">
+        <div className="popup\_inner">
+          <form id="popup-form">
+            <label htmlFor="username">Your Name: </label>
+            <input name="username"></input>
+            <button onClick={this.startCouch}>Start New Couch</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
