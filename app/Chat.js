146ffb6df33eaa24f4e0fbfc69d73b10c8c4ceaf
gkmurray124 <gkmurray124@gmail.com>
2020-03-27 12:38:32 -0400

    chore: socket.io

--- app/Chat.js
+++ app/Chat.js
@@ -1,64 +1,62 @@
-import React from "react";
-import socketIOClient from "socket.io-client";
+import React from 'react';
+import socketIOClient from 'socket.io-client';
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      couchId: "",
-      username: "place holder",
-      message: "",
-      messages: []
+      couchId: '',
+      username: '',
+      message: '',
+      messages: [],
     };
-    this.socket = socketIOClient(`http://localhost:3000/${this.state.couchId}`);
-    this.socket.on("chat-message", message => {
-      // addMessage(msg);
-      console.log("receiving message", message);
-      this.setState({ messages: [...this.state.messages, message] });
+    this.endpoint = 'http://localhost:3000'
+    this.socket = socketIOClient(this.endpoint, { query: `couchId=${this.props.couchId}`});
+    this.socket.on('receive message', msg => {
+      addMessage(msg);
     });
-    this.socket.on("new-user", username => {
-      this.socket.join(this.state.couchId);
+    this.socket.on('user-connected', username => {
       const msg = `${username} connected`;
       this.setState({ messages: [...this.state.messages, msg] });
     });
-    this.socket.on("disconnect", username => {
+    this.socket.on('couch-created', () => {
+      const msg = `${this.state.username} started the couch!`
+      this.setState({ messages: [...this.state.messages, msg] })
+     });
+    this.socket.on('user-disconnected', username => {
       const msg = `${username} disconnected`;
       this.setState({ messages: [...this.state.messages, msg] });
     });
 
-    // const addMessage = msg => {
-    //   this.setState({ messages: [...this.state.messages, msg] });
-    // };
+    const addMessage = msg => {
+      this.setState({ messages: [...this.state.messages, msg] });
+    };
 
     this.sendMessage = event => {
       event.preventDefault();
-      this.socket.emit(
-        "send-chat-message",
-        this.state.couchId,
-        this.state.username,
-        this.state.message
-      );
-      this.setState({ message: "" });
+      this.socket.emit('send-chat-message', this.state.couchId, this.state.username, this.state.message);
+      this.setState({ message: '' });
     };
   }
 
   componentDidMount() {
     //axios.get
+    console.log(this.props)
     if (this.props.location.state) {
-      console.log("hit this");
+      console.log('hit this')
       const joinCouch = { ...this.props.location.state };
       this.setState({ ...joinCouch });
     } else {
-      console.log("hit THIS", this.props.location);
+      console.log('hit THIS', this.props.location)
       const url = window.location.href;
-      const splitURL = url.split("/");
+      const splitURL = url.split('/');
       const couchId = splitURL[3];
       this.setState({ couchId: couchId });
     }
   }
 
   render() {
-    console.log("this.state__________", this.state);
+    console.log('this.state__________', this.state);
     return (
       <div>
         <h3>Share this Couch ID: {this.state.couchId}</h3>
@@ -66,8 +64,7 @@ export default class Chat extends React.Component {
           {this.state.messages.map(message => {
             return (
               <li>
-                {/* {message.username}: {message.message} */}
-                {message}
+                {this.state.username}: {message}
               </li>
             );
           })}
