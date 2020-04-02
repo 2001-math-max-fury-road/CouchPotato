ecfd531808a2a894fd32a70e7fd09d0eeabc7b6c
gkmurray124 <gkmurray124@gmail.com>
2020-04-02 18:25:50 -0400

    feature: adding emoji popup to chat form

--- app/Chat.js
+++ app/Chat.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import Socket from './Socket';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import Notifications, { notify } from 'react-notify-toast';
+import React from "react";
+import Socket from "./Socket";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Notifications, { notify } from "react-notify-toast";
 import "../public/emoji-mart.css";
 import { Picker } from "emoji-mart";
 
@@ -9,21 +9,22 @@ export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: '',
+      message: "",
       messages: [],
-      users: [], 
+      users: [],
+      emoji: ""
     };
     this.copiedToClipboard = this.copiedToClipboard.bind(this);
     this.showEmojis = this.showEmojis.bind(this);
     this.closeMenu = this.closeMenu.bind(this);
     this.addEmoji = this.addEmoji.bind(this);
 
-    Socket.on('user-connected', (username, users) => {
+    Socket.on("user-connected", (username, users) => {
       const message = `${username} joined the Couch`;
       this.setState({ messages: [...this.state.messages, message], users });
     });
 
-    Socket.on('user-disconnected', (socket, couch) => {
+    Socket.on("user-disconnected", (socket, couch) => {
       const username = couch[socket];
       const message = `${username} has left the Couch`;
       delete couch[socket];
@@ -34,7 +35,7 @@ export default class Chat extends React.Component {
       });
     });
 
-    Socket.on('receive-message', msgObj => {
+    Socket.on("receive-message", msgObj => {
       this.setState({ messages: [...this.state.messages, msgObj] });
       window.scrollTo(0, document.body.scrollHeight);
     });
@@ -42,41 +43,42 @@ export default class Chat extends React.Component {
     this.sendMessage = event => {
       event.preventDefault();
       Socket.emit(
-        'send-chat-message',
+        "send-chat-message",
         this.state.message,
         localStorage.username,
         localStorage.couchId
       );
-      this.setState({ message: '' });
+      this.setState({ message: "" });
       window.scrollTo(0, document.body.scrollHeight);
     };
-    
-    this.sendShot = event => { 
+
+    this.sendShot = event => {
       event.preventDefault();
-      Socket.emit('send-shot', 
-      "Everyone drink!", 
-      localStorage.username,
-      localStorage.couchId
-      )
+      Socket.emit(
+        "send-shot",
+        "Everyone drink!",
+        localStorage.username,
+        localStorage.couchId
+      );
       window.scrollTo(0, document.body.scrollHeight);
-    }
+    };
   }
 
   componentDidMount() {
-    const username = localStorage.getItem('username');
-    const couchId = localStorage.getItem('couchId');
-    Socket.emit('new-user', couchId, username);
+    const username = localStorage.getItem("username");
+    const couchId = localStorage.getItem("couchId");
+    Socket.emit("new-user", couchId, username);
   }
 
   componentWillUnmount() {
-    Socket.emit('disconnect');
+    Socket.emit("disconnect");
   }
 
   copiedToClipboard() {
-    const alertColor = { background: '#119da4', text: '#c8c8c8' };
+    const alertColor = { background: "#119da4", text: "#c8c8c8" };
     notify.show(
-      'Copied Couch ID to clipboard! Now share it with your friends.',
-      'custom',
+      "Copied Couch ID to clipboard! Now share it with your friends.",
+      "custom",
       5000,
       alertColor
     );
@@ -89,9 +91,9 @@ export default class Chat extends React.Component {
       },
       () => document.addEventListener("click", this.closeMenu)
     );
-  };
+  }
 
-  closeMenu (e) {
+  closeMenu(e) {
     if (this.emojiPicker !== null && !this.emojiPicker.contains(e.target)) {
       this.setState(
         {
@@ -100,17 +102,17 @@ export default class Chat extends React.Component {
         () => document.removeEventListener("click", this.closeMenu)
       );
     }
-  };
+  }
 
   addEmoji(e) {
     let emoji = e.native;
     this.setState({
       message: this.state.message + emoji
     });
-  };
+  }
 
   render() {
-    const users = this.state.users.join(', ');
+    const users = this.state.users.join(", ");
     return (
       <div id="outer-container">
         <Notifications />
@@ -147,7 +149,23 @@ export default class Chat extends React.Component {
                 }
               })}
             </ul>
-          </div>
+          </div>  
+          <div id="flex-container">
+            <div id="emoji">
+              {this.state.showEmojis ? (
+                <span id="emoji-span" ref={el => (this.emojiPicker = el)}>
+                  <Picker onSelect={this.addEmoji} emojiTooltip={true} />
+                </span>
+              ) : (
+                <img
+                  id="emoji-img"
+                  src={
+                    "https://cdn.shopify.com/s/files/1/1061/1924/products/Emoji_Icon_-_Cowboy_emoji_large.png?v=1571606089"
+                  }
+                  onClick={this.showEmojis}
+                ></img>
+              )}
+            </div>
           <form id="chat-form" action="">
             <input
               type="text"
@@ -155,22 +173,18 @@ export default class Chat extends React.Component {
               value={this.state.message}
               onChange={ev => this.setState({ message: ev.target.value })}
               className="form-control"
-            />
-                    {this.state.showEmojis ? (
-          <span ref={el => (this.emojiPicker = el)}>
-            <Picker
-              onSelect={this.addEmoji}
-              emojiTooltip={true}
-            />
-          </span>
-        ) : (
-          <img id="emoji-img" src={"https://cdn.shopify.com/s/files/1/1061/1924/products/Emoji_Icon_-_Cowboy_emoji_large.png?v=1571606089"}onClick={this.showEmojis}></img>
-        )}
+              />
             <button onClick={this.sendMessage}>Send</button>
-            <img src={'https://images.vexels.com/media/users/3/143358/isolated/preview/0fb2d717f3362970778533776849ec50-tequila-shot-icon-by-vexels.png'} onClick={this.sendShot}></img>
+            <img
+              src={
+                "https://images.vexels.com/media/users/3/143358/isolated/preview/0fb2d717f3362970778533776849ec50-tequila-shot-icon-by-vexels.png"
+              }
+              onClick={this.sendShot}
+              ></img>
           </form>
-        </div>
-      </div>
+          </div>
+          </div>       
+    </div>
     );
   }
 }
