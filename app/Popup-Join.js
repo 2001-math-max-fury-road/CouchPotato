0970410b12ecdfa5dfa8e4fe706fe0f163c72573
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2020-04-04 11:15:02 -0400

    bug: fix for heroku deployment

--- app/Popup-Join.js
+++ app/Popup-Join.js
@@ -21,10 +21,12 @@ export default class JoinPopup extends React.Component {
 
   joinCouch(event) {
     event.preventDefault();
-    localStorage.setItem("couchId", this.state.couchId);
-    localStorage.setItem("username", this.state.username);
+    localStorage.setItem('couchId', this.state.couchId);
+    localStorage.setItem('username', this.state.username);
     // location.replace(`http://localhost:3000/${this.state.couchId}`);
-    location.replace(`http://couch-potato-extension.herokuapp.com/${this.state.couchId}`);
+    location.replace(
+      `http://couch-potato-extension.herokuapp.com/${this.state.couchId}`
+    );
   }
 
   async chooseAvatar(event) {
