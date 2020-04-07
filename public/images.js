5c68342f3f9502337e0bce373ca598a5a773583f
Caitlin Floyd <caitlinfloyd+coding@gmail.com>
2020-04-07 17:57:39 -0400

    chore: update image links

--- public/images.js
+++ public/images.js
@@ -2,85 +2,85 @@ const images = [
   {
     id: 1,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/potato.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/potato.png?raw=true',
     title: 'potato',
   },
   {
     id: 2,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/pizza.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/pizza.png?raw=true',
     title: 'pizza',
   },
   {
     id: 3,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/wine.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/wine.png?raw=true',
     title: 'wine',
   },
   {
     id: 4,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/dino.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/dino.png?raw=true',
     title: 'dinosaur',
   },
   {
     id: 5,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/dog.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/dog.png?raw=true',
     title: 'dog',
   },
   {
     id: 6,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/cat.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/cat.png?raw=true',
     title: 'cat',
   },
   {
     id: 7,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/koala.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/koala.png?raw=true',
     title: 'koala',
   },
   {
     id: 8,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/bunny.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/bunny.png?raw=true',
     title: 'bunny',
   },
   {
     id: 9,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/shiba.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/shiba.png?raw=true',
     title: 'shiba',
   },
   {
     id: 10,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/whale.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/whale.png?raw=true',
     title: 'whale',
   },
   {
     id: 11,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/turtle.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/turtle.png?raw=true',
     title: 'turtle',
   },
   {
     id: 12,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/narwhal.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/narwhal.png?raw=true',
     title: 'narwhal',
   },
   {
     id: 13,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/penguin.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/penguin.png?raw=true',
     title: 'penguin',
   },
   {
     id: 14,
     src:
-      'https://github.com/2001-math-max-fury-road/CouchPotato/tree/master/public/avatar-images/alien.png?raw=true',
+      'https://github.com/2001-math-max-fury-road/CouchPotato/blob/master/public/avatar-images/alien.png?raw=true',
     title: 'alien',
   },
 ];
