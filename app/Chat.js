909772872128f702965383b7bc080a85b99fb5a7
eunjoon <joonybejoy@gmail.com>
2020-05-03 16:42:18 -0400

    chore:imagelink fix

--- app/Chat.js
+++ app/Chat.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Socket from './Socket';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import Notifications, { notify } from 'react-notify-toast';
-import '../public/emoji-mart.css';
-import { Picker } from 'emoji-mart';
-import UserForm from './UserForm';
-import Popup from 'reactjs-popup';
+import React from "react";
+import Socket from "./Socket";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Notifications, { notify } from "react-notify-toast";
+import "../public/emoji-mart.css";
+import { Picker } from "emoji-mart";
+import UserForm from "./UserForm";
+import Popup from "reactjs-popup";
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: '',
+      message: "",
       messages: [],
       users: [],
     };
@@ -21,12 +21,12 @@ export default class Chat extends React.Component {
     this.closeMenu = this.closeMenu.bind(this);
     this.addEmoji = this.addEmoji.bind(this);
 
-    Socket.on('user-connected', (username, users) => {
+    Socket.on("user-connected", (username, users) => {
       const message = `${username} joined the Couch`;
       this.setState({ messages: [...this.state.messages, message], users });
     });
 
-    Socket.on('user-disconnected', (socket, couch) => {
+    Socket.on("user-disconnected", (socket, couch) => {
       const username = couch[socket];
       const message = `${username} has left the Couch`;
       delete couch[socket];
@@ -37,13 +37,13 @@ export default class Chat extends React.Component {
       });
     });
 
-    Socket.on('receive-message', msgObj => {
+    Socket.on("receive-message", (msgObj) => {
       this.setState({ messages: [...this.state.messages, msgObj] });
       window.scrollTo(0, document.body.scrollHeight);
     });
 
-    Socket.on('player', (huluID, message, status) => {
-      window.top.postMessage(`play-pause ${status} ${huluID}`, '*');
+    Socket.on("player", (huluID, message, status) => {
+      window.top.postMessage(`play-pause ${status} ${huluID}`, "*");
       this.setState({ messages: [...this.state.messages, message] });
       window.scrollTo(0, document.body.scrollHeight);
     });
@@ -51,25 +51,25 @@ export default class Chat extends React.Component {
     this.sendMessage = (event) => {
       event.preventDefault();
       Socket.emit(
-        'send-chat-message',
+        "send-chat-message",
         this.state.message,
         localStorage.username,
         localStorage.avatar,
         localStorage.couchId
       );
-      const allMessages = document.getElementsByClassName('message');
+      const allMessages = document.getElementsByClassName("message");
       const messageArray = Array.from(allMessages);
       messageArray.map((msg) => {
-        msg.removeAttribute('tab blink');
+        msg.removeAttribute("tab blink");
       });
-      this.setState({ message: '' });
+      this.setState({ message: "" });
       window.scrollTo(0, document.body.scrollHeight);
     };
 
     this.sendShot = (event) => {
       event.preventDefault();
       Socket.emit(
-        'send-shot',
+        "send-shot",
         localStorage.username,
         localStorage.avatar,
         localStorage.couchId
@@ -79,21 +79,21 @@ export default class Chat extends React.Component {
   }
 
   componentDidMount() {
-    const username = localStorage.getItem('username');
-    const couchId = localStorage.getItem('couchId');
-    window.top.postMessage(`couchID ${couchId} ${username}`, '*');
-    Socket.emit('new-user', couchId, username);
+    const username = localStorage.getItem("username");
+    const couchId = localStorage.getItem("couchId");
+    window.top.postMessage(`couchID ${couchId} ${username}`, "*");
+    Socket.emit("new-user", couchId, username);
   }
 
   componentWillUnmount() {
-    Socket.emit('disconnect');
+    Socket.emit("disconnect");
   }
 
   copiedToClipboard() {
-    const alertColor = { background: '#119da4', text: '#c8c8c8' };
+    const alertColor = { background: "#119da4", text: "#c8c8c8" };
     notify.show(
-      'Copied Couch ID to clipboard! Now share it with your friends.',
-      'custom',
+      "Copied Couch ID to clipboard! Now share it with your friends.",
+      "custom",
       5000,
       alertColor
     );
@@ -104,7 +104,7 @@ export default class Chat extends React.Component {
       {
         showEmojis: true,
       },
-      () => document.addEventListener('click', this.closeMenu)
+      () => document.addEventListener("click", this.closeMenu)
     );
   }
 
@@ -114,7 +114,7 @@ export default class Chat extends React.Component {
         {
           showEmojis: false,
         },
-        () => document.removeEventListener('click', this.closeMenu)
+        () => document.removeEventListener("click", this.closeMenu)
       );
     }
   }
@@ -127,7 +127,7 @@ export default class Chat extends React.Component {
   }
 
   render() {
-    const users = this.state.users.join(', ');
+    const users = this.state.users.join(", ");
     return (
       <div id="outer-container">
         <Notifications />
@@ -174,11 +174,11 @@ export default class Chat extends React.Component {
                 if (message.username && !message.message) {
                   return (
                     <li className="message" class="tab blink">
-                      <img src={message.avatar} />{' '}
+                      <img src={message.avatar} />{" "}
                       <div id="drinking-game">
                         <div id="take-a-drink">
-                          {message.username} says take a drink!{' '}
-                          <img src="https://images.vexels.com/media/users/3/143358/isolated/preview/0fb2d717f3362970778533776849ec50-tequila-shot-icon-by-vexels.png" />{' '}
+                          {message.username} says take a drink!{" "}
+                          <img src="https://images.vexels.com/media/users/3/143358/isolated/preview/0fb2d717f3362970778533776849ec50-tequila-shot-icon-by-vexels.png" />{" "}
                           Cheers!
                         </div>
                       </div>
@@ -187,9 +187,9 @@ export default class Chat extends React.Component {
                 } else if (message.username) {
                   return (
                     <li>
-                      <img id="avatar" src={message.avatar} />{' '}
+                      <img id="avatar" src={message.avatar} />{" "}
                       <div id="message-content">
-                        {message.username}: {message.message}{' '}
+                        {message.username}: {message.message}{" "}
                       </div>
                     </li>
                   );
@@ -208,9 +208,7 @@ export default class Chat extends React.Component {
               ) : (
                 <img
                   id="emoji-img"
-                  src={
-                    'https://github.com/2001-math-max-fury-road/CouchPotato/blob/avatar-images/public/avatar-images/cowboy.png?raw=true'
-                  }
+                  src="/avatar-images/cowboy.png?raw=true"
                   onClick={this.showEmojis}
                 ></img>
               )}
@@ -226,9 +224,7 @@ export default class Chat extends React.Component {
               <button onClick={this.sendMessage}>Send</button>
               <img
                 id="drink-icon"
-                src={
-                  'https://github.com/2001-math-max-fury-road/CouchPotato/blob/avatar-images/public/avatar-images/drink-icon.png?raw=true'
-                }
+                src="/avatar-images/drink-icon.png?raw=true"
                 onClick={this.sendShot}
               ></img>
             </form>
